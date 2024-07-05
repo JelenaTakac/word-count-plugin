306b4a86a53ccf4f0ff728e86a8af40f64cb998a
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-07-05 13:51:19 +0200

    Displaying Post Statistics (words count, characters count and read time of post)

--- our-first-unique-plugin.php
+++ our-first-unique-plugin.php
@@ -24,6 +24,44 @@ class WordCountAndTimePlugin
     {
         add_action('admin_menu', array($this, 'adminPage')); // 'admin_menu' - Fires before the administration menu loads in the admin.
         add_action('admin_init', array($this, 'settings')); // 'admin_init' is triggered before any other hook when a user accesses the admin area.
+        add_filter('the_content', array($this, 'ifWrap'));
+    }
+
+    function ifWrap($content) {
+        if (is_single() AND is_main_query() AND 
+        (get_option('wcp_wordcount', '1') OR
+            get_option('wcp_charactercount', '1') OR
+            get_option('wcp_readtime', '1')
+        )) {
+            return $this->createHTML($content);
+        } 
+        return $content;
+    }
+
+    function createHTML($content) {
+        $html = '<h3>'  . esc_html(get_option('wcp_headline', 'Post Statistics')) .  '</h3><p>';
+
+        if (get_option('wcp_wordcount', '1') OR get_option('wcp_readtime', '1')) {
+            $wordCount = str_word_count(strip_tags($content));
+        }
+        
+        if (get_option('wcp_wordcount', '1')) {
+            $html .= 'This post has ' . $wordCount . ' words.<br>';
+        }
+
+        if (get_option('wcp_charactercount', '1')) {
+            $html .= 'This post has ' . strlen(strip_tags($content)) . ' characters.<br>';
+        }
+
+        if (get_option('wcp_readtime', '1')) {
+            $html .= 'This post will take about ' . round($wordCount/225) . ' minute(s) to read.<br>';
+        }
+
+        if (get_option('wcp_location', '0') == '0') {
+            // on beggining
+            return $html . $content;
+        } 
+        return $content . $html;
     }
 
     function settings()
